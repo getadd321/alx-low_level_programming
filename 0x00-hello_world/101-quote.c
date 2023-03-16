7144220fec086391a14879ca9596e84ff7908d52
getadd321 <ghiwotadane@gmail.com>
2023-03-16 15:23:18 -0700

    print using fputs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
- * main - Entry point
+ *main - Entry point
  *
- * Return: Always 1 (Success)
+ *Return: Always 1 (Success)
  */
 int main(void)
 {
 	fputs("and that piece of art is useful\" - \
 Dora Korpar, 2015-10-19\n", stderr);
-=======
- *main -Entry point
- *Return: 1 (Success)
- */
-int main(void)
-{
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
->>>>>>> 8caa63a602e460d140a7a276de3bdea975bac3ed
-	return (1);
 }
