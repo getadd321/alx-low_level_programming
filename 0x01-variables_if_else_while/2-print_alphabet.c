94c1cd8199518f5367a8fb574901fb67269a0c41
getadd321 <ghiwotadane@gmail.com>
2023-03-17 07:16:53 -0700

    putchar alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
-#include<stdilib.h>
 /**
- *
- *main -Entry point
- *Return: Always 0 (Success)
-
- */
-int main (void)
+* main -Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
+	char c;
 
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
