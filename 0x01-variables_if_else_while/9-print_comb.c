e52be6748a92de77ea406d157ce4209ba06d2a25
getadd321 <ghiwotadane@gmail.com>
2023-03-17 13:13:04 -0700

    add comma and space to nums

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,8 @@ int main(void)
 	for (i = 0; i < 10;  i++)
 	{
 		putchar(i + '0');
-		putchar(i < 9 ? ',' : '\n');
+		putchar(i < 9 ? ',' : '$');
 		putchar(i < 9 ? ' ' : '\n');
 	}
-	putchar('\r');
 	return (0);
 }
