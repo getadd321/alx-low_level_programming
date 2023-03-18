63f6e1ed6eacced38a6b4696953146a98c0d361e
getadd321 <ghiwotadane@gmail.com>
2023-03-18 06:02:25 -0700

    four digit combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main -Entry point
+*main -Entry point
 *
-* Return: Always 0 (Success)
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-	int i, j, k, p;
+	int i, j;
 
-	for (i = 0; i < 10;  i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			for (k = 0 ; k < 10; k++)
-			{
-				for (p = 0; p < 10; p++)
-				{
-					putchar('0' + i);
-					putchar('0' + j);
-					putchar(' ');
-					putchar('0' + k);
-					putchar('0' + p);
-					if (i == 9 && j == 8 && k == 9 && p == 9)
-						continue;
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 98 && j == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
