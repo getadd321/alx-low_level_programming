42d3c56054ec47e49888e676b6c9cd7e43078cc6
getadd321 <ghiwotadane@gmail.com>
2023-03-21 15:48:12 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,9 +4,8 @@
 *
 * @n: is an integer
 *
-* Return: 1 and prints + if n is greater than zero.
-* 0 and prints 0 if n is zero.
-* -1 and prints - if n is less than zero.
+* Return: 1 if n is greater than zero. 0 if n is zero.
+* -1 ` n is less than zero.
 */
 int print_sign(int n)
 {
@@ -16,7 +15,7 @@ int print_sign(int n)
 	else if (n == 0)
 		_putchar('0');
 		return (0);
-	else if (n < 0)
+	else
 		_putchar('-');
 		return (-1);
 	_putchar('\n');
