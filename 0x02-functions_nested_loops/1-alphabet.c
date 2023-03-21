54109c29ab5f9de7fced80959504f8eda2acfd66
getadd321 <ghiwotadane@gmail.com>
2023-03-21 11:35:58 -0700

    1. I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - prints alphabets in lower-case
+*
+* Return: 0 Always(Success)
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c >= 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
