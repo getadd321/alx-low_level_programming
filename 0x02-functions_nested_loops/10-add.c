8cdb12433db1f722db5bb0b33af7d2a22442f15f
getadd321 <ghiwotadane@gmail.com>
2023-03-21 16:43:05 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* add - Calculates the sum of two input numbers
+*
+*@a: is an integer
+*@b: is an integer
+*
+* Return: sum of input numbers
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
