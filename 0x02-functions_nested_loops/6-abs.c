2cac253fb6aac6db7c4f9d0f06de4d3a913b76da
getadd321 <ghiwotadane@gmail.com>
2023-03-21 16:15:14 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _abs - Gives the absolutevalue of an input integer
+*
+*@i: is an integer
+*
+* Return: absolute value
+*/
+int _abs(int i)
+{
+	if (i > 0)
+		return (i);
+	return (i * -1);
+}
