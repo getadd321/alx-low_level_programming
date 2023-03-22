cfc27dd2d64b54076ef044283b63a6cb60429596
getadd321 <ghiwotadane@gmail.com>
2023-03-21 17:15:38 -0700

    minutes of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,10 +9,15 @@ void jack_bauer(void)
 	int i, j, k, l;
 
 	for (i = 0; i <= 2; i++)
+	{
 		for (j = 0; j <= 9; j++)
+		{
 			for (k = 0; k <= 5; k++)
+			{
 				for (l = 0; l <= 9; l++)
+				{
 					if (i >= 2 && j >= 4)
+					{
 						break;
 					_putchar(i + '0');
 					_putchar(j + '0');
@@ -20,4 +25,10 @@ void jack_bauer(void)
 					_putchar(k + '0');
 					_putchar(l + '0');
 					_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+
 }
