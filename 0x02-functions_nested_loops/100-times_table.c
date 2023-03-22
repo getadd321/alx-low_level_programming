302eb85bd3bd8e2ade62e5374f8511be6e22c777
getadd321 <ghiwotadane@gmail.com>
2023-03-22 07:27:22 -0700

    15x time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_times_table - prints the 9 times table, starting with 0
+*
+*@n: is positive integer
+*
+* Return: nothing
+*/
+void print_times_table(int n)
+{
+	int i, j, r, m, d;
+
+	if (n > 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				r = i * j;
+
+				if (r > 9)
+				{
+					m = r % 10;
+					d = (r - m) / 10;
+					printf(", %d%d", d, m);
+				}
+				else
+				{
+					if (j != 0)
+					{
+					printf(",  ");
+					}
+					printf("%d", r);
+				}
+			}
+			printf("\n");
+		}
+	}
+	printf("\n");
+}
