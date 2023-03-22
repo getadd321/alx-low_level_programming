a81c7116e37743b1807a82610df0e78ee64054a1
getadd321 <ghiwotadane@gmail.com>
2023-03-21 17:10:40 -0700

    minutes of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* jack_bauer - Prints every minute of the day from 00:00 to 23:59
+*
+* Return: nothing
+*/
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+		for (j = 0; j <= 9; j++)
+			for (k = 0; k <= 5; k++)
+				for (l = 0; l <= 9; l++)
+					if (i >= 2 && j >= 4)
+						break;
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+}
