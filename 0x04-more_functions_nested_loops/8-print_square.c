00c672ba962470681f7722a74cd83dd146814975
getadd321 <ghiwotadane@gmail.com>
2023-03-23 15:32:22 -0700

    8. You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints stright line
+ * print_square - prints square #
  *
- *@n: input number
+ *@size: input number
  * Return: nothing
  */
-void print_diagonal(int n)
+void print_square(int size)
 {
 	int i, j;
 
-	if (n > 0)
+	if (size > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < (n - 1); j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
-			_putchar('#');
 			_putchar('\n');
 		}
 	}
