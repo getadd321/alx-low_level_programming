7669fa035d518167e84184014c87f677bde75932
getadd321 <ghiwotadane@gmail.com>
2023-03-23 15:36:24 -0700

    8. You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,10 +14,11 @@ void print_square(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < size; j++)
+			for (j = 0; j < (size - 1); j++)
 			{
 				_putchar('#');
 			}
+			_putchar('#');
 			_putchar('\n');
 		}
 	}
