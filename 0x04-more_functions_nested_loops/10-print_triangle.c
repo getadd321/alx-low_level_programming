9baf71d986f8471510c5cb358699edd1493bb784
getadd321 <ghiwotadane@gmail.com>
2023-03-23 16:42:48 -0700

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle #
+ *
+ *@size: input number
+ *
+ * Return: nothing
+*/
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for ((j = size - i); j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			if (i == size)
+			{
+				continue;
+			}
+		}
+	}
+	_putchar('\n');
+}
