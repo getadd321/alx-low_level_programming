4cb61bf9e759e109014753f5f2e8236dfc2c79d8
getadd321 <ghiwotadane@gmail.com>
2023-03-24 13:39:38 -0700

    prime numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,33 +3,25 @@
  * main -Entry point
  *
  * Return: 0 Always(Success)
-*/
-
-#include <math.h>
-
+ */
 int main(void)
 {
-	long long num = 61285247514
-	long long max_factor = -1;
+	long num = 612852475143;
+	int  i = 1;
 
-	while (num % 2 == 0)
+	while (i++ < num / 2)
 	{
-		max_factor = 2;
-		num /= 2;
-	}
-	for (long long i = 3; i <= sqrt(num); i += 2)
-	{
-		while (num % i == 0)
+		if (num % i == 0)
 		{
-			max_factor = i;
-			num /= i;
+			num /= factor;
+			continue;
+		}
+		for (i = 3 ; i < num / 2; i += 2)
+		{
+			if (num % i == 0)
+				num /= i;
 		}
 	}
-	if (num > 2)
-	{
-		max_factor = num;
-	}
-	printf("%lld\n", max_factor);
+	printf("%ld\n", num);
 	return (0);
 }
-
