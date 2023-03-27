e64b25a9666809bcd8b55b7221bbe4ff1e64d7f4
getadd321 <ghiwotadane@gmail.com>
2023-03-27 07:57:33 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_rev - prints string to stdout in reverse
+ *
+ *@s: the input string
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int s_len = 0;
+	int s_counter;
+
+	while (*s != '\0')
+	{
+		*s++;
+		s_counter++;
+	}
+	s--;
+	for (s_len = s_counter; s_len > 0; s_len--)
+	{
+		_putchar(*s);
+		s--;
+
+	}
+
+}
