16fb44d58299884699f957a49d024a7953730c2f
getadd321 <ghiwotadane@gmail.com>
2023-03-27 03:48:14 -0700

    swap  values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+/**
+ * swap_int - swaps the input numbers
+ *
+ *@a: input number
+ *@b: input number
+ *
+ * Return: nothing.
+ */
+void swap_int(int *a, int *b)
+{
+	int n = *a;
+
+	*a = *b;
+	*b = n;
+
+}
