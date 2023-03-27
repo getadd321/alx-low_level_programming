1da5afea7b3ca584a46830790a2711b3b13e116c
getadd321 <ghiwotadane@gmail.com>
2023-03-27 06:10:58 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+/**
+ * _puts - checks the length of string
+ *
+ *@str:input string
+ *
+ * Return: lenght
+ */
+void _puts(char *str)
+{
+	int str_len = 0;
+
+	while (*s != '\0')
+	{
+		_putchar(str++);
+	}
+	_putchar('\n');
+
+}
