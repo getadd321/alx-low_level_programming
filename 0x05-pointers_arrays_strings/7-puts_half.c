7bac5433bf2d1281c0a2045a2715e8979294a937
getadd321 <ghiwotadane@gmail.com>
2023-03-27 14:04:26 -0700

    print the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * puts_half - prints half of string to stdout
+ *
+ *@str: the input string
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int s_len = 0;
+	char *s = str;
+	int i;
+	int n;
+
+	while (*s != '\0')
+	{
+		s++;
+		s_len++;
+	}
+	if(s_len % 2 == 0)
+	{
+		n = s_len / 2;
+	}
+	n = (s_len -1) / 2;
+
+	for (i = n; i < s_len; i++)
+	{
+		_putchar(str[i]);
+
+	}
+	_putchar('\n');
+
+}
