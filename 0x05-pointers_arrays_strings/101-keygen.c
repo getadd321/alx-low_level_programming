c6b6ef59fe345299cd4cc7c116cf89bfcc983c0f
getadd321 <ghiwotadane@gmail.com>
2023-03-28 15:40:25 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,26 +10,26 @@
  **/
 int main(void)
 {
+	int pass[100];
+	int i, sum, n;
 	const int PASSWORD_LENGTH = 100;
-	const int PASSWORD_SUM_CONSTRAINT = 2772;
-	const char CHARSET[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+{}[]|;:<>,.?/";
-	const int CHARSET_SIZE = sizeof(CHARSET) - 1; 
-	int pass[PASSWORD_LENGTH];
-	int i, sum = 0, n;
+    	const int CHAR_SET_SIZE = 78;
+
+    	sum = 0;
 	srand(time(NULL));
-	for (i = 0; i < PASSWORD_LENGTH; i++)
+    	for (i = 0; i < PASSWORD_LENGTH; i++)
 	{
-		pass[i] = rand() % CHARSET_SIZE;
-		sum += (pass[i] + '0');
-		putchar(CHARSET[pass[i]]);
-		if ((PASSWORD_SUM_CONSTRAINT - sum) - '0' < CHARSET_SIZE)
-		{
-			n = PASSWORD_SUM_CONSTRAINT - sum - '0';
-			sum += n;
-			putchar(CHARSET[n]);
-			break;
-		}
-	}
-	return 0;
+		pass[i] = rand() % CHAR_SET_SIZE;
+        	sum += (pass[i] + '0');
+        	putchar(pass[i] + '0');
+        	if ((2772 - sum) - '0' < CHAR_SET_SIZE) {
+            	n = 2772 - sum - '0';
+            	sum += n;
+           	putchar(n + '0');
+            	break;
+        }
+    }
+    putchar('\n');
+    return (0);
 }
 
