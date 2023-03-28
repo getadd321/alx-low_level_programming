bf785bb88152138b8e5b049bcd26d744306b2ee3
getadd321 <ghiwotadane@gmail.com>
2023-03-28 09:33:20 -0700

    print the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -21,9 +21,9 @@ void puts_half(char *str)
 		str--;
 	}
 	n =  (s_len % 2 == 0) ? s_len / 2 : (s_len - 1) / 2;
-	for ( ; n < s_len; s_len++)
+	for (; n < s_len; s_len++)
 	{
-		_putchar(*str);
+		_putchar(str[n]);
 	}
 	_putchar('\n');
 }
