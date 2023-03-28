5978603d428ccea75c1dcf1bb147b5c088de1a94
getadd321 <ghiwotadane@gmail.com>
2023-03-28 12:32:12 -0700

    print digits

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,18 +16,15 @@ int _atoi(char *s)
 	{
 		if (*s == '-')
 		{
-			sign = -1;
+			sign *= -1;
 		}
-		else if (*s == '+')
+		while (*s >= '0' && *s <= '9')
 		{
-			sign = 1;
+			digit = 1;
+			result = (result * 10) + (*s - '0');
+			s++;
 		}
-		else if (*s >= '0' && *s <= '9')
-		{
-			digit = *s - '0';
-			result = result * 10 + digit;
-		}
-		else if (result != 0)
+		if (digit == 1)
 		{
 			break;
 		}
