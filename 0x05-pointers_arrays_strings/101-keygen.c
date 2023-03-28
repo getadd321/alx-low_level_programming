4378e457c43f1ae0d6a36ca9624eb31ae993b447
getadd321 <ghiwotadane@gmail.com>
2023-03-28 14:44:19 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,10 +11,13 @@
 int main(void)
 {
 	int p_len = 8;
+	int i;
 	char password[p_len + 1];
 	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()_+{}[]<>?,./";
 	int chaset_size = sizeof(charset) - 1;
+	
 	srand(time(NULL));
+	
 	for (i = 0; i < _plen; i++)
 	{
 		password[i] = charset[rand() % charset_size];
