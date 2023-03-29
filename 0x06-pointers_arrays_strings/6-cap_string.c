6fcf98f01eb4434baefb04c86378dd0b4c3ea7ce
getadd321 <ghiwotadane@gmail.com>
2023-03-29 12:35:47 -0700

    capitalizes all words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+*cap_string - capitalizes all words of a string.
+*
+*@str: string input
+*
+*
+*Return: string
+*
+**/
+char *cap_string(char *str)
+{
+	char *ptr = str;
+	int is_new_word = 1;
+
+	while (*ptr != '\0')
+	{
+		if (is_new_word && (*ptr >= 'a' && *ptr <= 'z'))
+		{
+			*ptr = *ptr - 'a' + 'A';
+		}
+		is_new_word = (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == ',' ||
+				*ptr == ';' || *ptr == '.' || *ptr == '!' || *ptr == '?' ||
+				*ptr == '"' || *ptr == '(' || *ptr == ')' || *ptr == '{' || *ptr == '}');
+		if (*ptr == '\t')
+		{
+			*ptr = ' ';
+		}
+		*ptr++;
+	}
+	return (str);
+}
