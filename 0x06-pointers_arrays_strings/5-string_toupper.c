d4680f8132fb243b7b0f89c92cf8a1aa243f2cc0
getadd321 <ghiwotadane@gmail.com>
2023-03-29 08:14:21 -0700

    TO UPPER

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*string_toupper - converst lowercase charactre to upper
+*
+*@str: string input
+*
+*
+*Return: upper case char
+*
+**/
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - 'a' + 'A';
+		}
+		*ptr++;
+	}
+	return (str);
+}
