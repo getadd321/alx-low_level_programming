e06f43fe4c22b53792804d88f136b65585210815
getadd321 <ghiwotadane@gmail.com>
2023-03-30 11:35:47 -0700

    encode string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*leet - encodes some characters
+*leet - encodes a string into 1337
 *
 *@str: string input
 *
@@ -11,24 +11,18 @@
 char *leet(char *str)
 {
 	char *ptr = str;
-	char char_encoder[26][2] = {{'a', '4'}, {'b', 'b'}, {'c', 'c'}
-		, {'d', 'd'}, {'e', '3'}, {'f', 'f'}, {'g', 'g'}
-		, {'h', 'h'}, {'i', 'i'}, {'j', 'j'}, {'k', 'k'}
-		, {'l', '1'}, {'m', 'm'}, {'n', 'n'}, {'o', '0'}
-		, {'p', 'p'}, {'q', 'q'}, {'r', 'r'}, {'s', 's'}
-		, {'t', '7'}, {'u', 'u'}, {'v', 'v'}, {'w', 'w'}
-		, {'x', 'x'}, {'y', 'y'}, {'z', 'z'}};
+	char code1[] = "aAeEoOtTlL";
+	char code2[] = "4433007711"
 
 	int i, j;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < 26; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (str[i] == char_encoder[j][0] || str[i] == char_encoder[j][0] - 32)
+			if (str[i] == code1[j])
 			{
-				str[i] = char_encoder[j][1];
-				break;
+				str[i] = code2[j];
 			}
 		}
 	}
