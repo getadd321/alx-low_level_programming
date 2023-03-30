1026962f8164bd694eec0486afcee855f2f34e17
getadd321 <ghiwotadane@gmail.com>
2023-03-30 02:54:20 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,11 +18,11 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		dest++;
 	}
-	for (i = 0; i < n && src[i] != '0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		*dest = src[i];
 		dest++;
 	}
-	dest = '\0';
+	*dest = '\0';
 	return (dest_ptr);
 }
