51aa250762537ed4f97a558ca4690fd6528371c5
getadd321 <ghiwotadane@gmail.com>
2023-03-30 03:59:56 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,11 @@
 **/
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (*s1 && (*s1 == *s2))
 	{
-		if (s1[i] != s2[i])
-		{
-			return ((int)(s1[i] - s2[i]));
-		}
-		i++;
+		s1++;
+		s2++;
 	}
-	return (s1[i] - s2[i]);
+	return ((unsigned char)(*s1) - (unsigned char)(*s2));
+}
+
