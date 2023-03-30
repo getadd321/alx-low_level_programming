443abd8023883c103d853543705d250109c692d8
getadd321 <ghiwotadane@gmail.com>
2023-03-30 14:06:33 -0700

    print num

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_number - prints an integer.
+*
+*@n: input integer
+*
+*
+*Return: nothing
+**/
+void print_number(int n)
+{
+	int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	if(n / 10 !=0)
+	{
+		_putchar(num / 10)
+	}
+	_putchar((num % 10) + '0');
+}
+
