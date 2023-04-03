b143b0ef3899ce23862010c2c78c2d94dfa16228
getadd321 <ghiwotadane@gmail.com>
2023-04-03 04:34:13 -0700

    2.strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: string input
+ * @c: charcter match
+ * Return: pointer to the char c
+ **/
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (n--)
+	{
+		if (s[i] == c)
+		return (&s[i]);
+		i++;
+	}
+	return (0);
+}
