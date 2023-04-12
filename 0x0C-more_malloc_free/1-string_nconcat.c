5874a18a639b8241939dd4e5ce31209f15d55da4
getadd321 <ghiwotadane@gmail.com>
2023-04-12 06:09:30 -0700

    concatenate n bytes of s2 to s1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,12 +22,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2++;
 	if (n >= len2)
 		n = len2;
-	result = malloc((len1 + n + 1) * sizeof(char));
+	result = malloc((len1 + n + 1) * sizeof(char);
 	if (result == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
 	for (j = 0; j < n; j++)
 		result[i + j] = s2[j];
+	result[i + j] = '\0';
 	return (result);
 }
