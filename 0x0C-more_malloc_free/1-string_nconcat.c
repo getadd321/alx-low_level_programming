aba9b41bc4b19f49c87467db4c0d7392af770b68
getadd321 <ghiwotadane@gmail.com>
2023-04-12 06:02:16 -0700

    concatenate n bytes of s2 to s1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates n bytes of a s2 to s1
+ * @s1: input string1
+ * @s2: input string2
+ * @n: number of bytes ro concatinate
+ * Return: pointer to the concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = 0, len2 = 0, i, j;
+	char *result;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+	if (n >= len2)
+		n = len2;
+	result = malloc((len1 + n + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < n; j++)
+		result[i + j] = s2[j];
+	return (result);
+}
