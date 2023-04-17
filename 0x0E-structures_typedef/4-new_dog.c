a8ee2140747a3fe5f2b7e7ebb151db20b0eaa8b8
getadd321 <ghiwotadane@gmail.com>
2023-04-17 10:06:17 -0700

    creates new struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,14 +22,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(ndog);
 		return (NULL);
 	}
-	ndog->name = malloc(sizeof(char) * (len_name + 1));
+	ndog->name = malloc(len_name + 1);
 	if (ndog->name == NULL)
 	{
 		free(ndog->name);
 		free(ndog);
 		return (NULL);
 	}
-	ndog->owner = malloc(sizeof(char) * (len_owner + 1));
+	ndog->owner = malloc(len_owner + 1);
 	if (ndog->owner == NULL)
 	{
 		free(ndog->name);
