d3bf734d4fac0a9b50afe263c42f73f52b5ecf3d
getadd321 <ghiwotadane@gmail.com>
2023-04-17 05:04:09 -0700

    simple struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*struct dog - defines dog object
+*
+* @name: name of the dog
+* @age: age of the dog
+* @owner: name of the owner of the dog
+**/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
