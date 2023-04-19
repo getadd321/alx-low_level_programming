434ba4b555ef5c36fc6c7353d0163c435101c3da
getadd321 <ghiwotadane@gmail.com>
2023-04-19 14:24:57 -0700

    prototypes and data structures of the program

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,12 +1,5 @@
 #ifndef CALC_H
 #define CALC_H
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 /**
  * struct op - Struct op
  *
@@ -19,5 +12,12 @@ typedef struct op
 	int (*f)(int a, int b);
 }
 op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
 #endif
