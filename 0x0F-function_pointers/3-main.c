e6fdd77d948ddaf15f9d0df0e2aa762d1def906b
getadd321 <ghiwotadane@gmail.com>
2023-04-19 14:38:00 -0700

    prints reult

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,12 +25,12 @@ int main(int argc, char *argv[])
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
 	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit(98);
+		exit(100);
 	}
 	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
