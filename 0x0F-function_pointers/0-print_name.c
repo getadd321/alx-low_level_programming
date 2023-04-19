f38b97926e0d5a3733a86413e0169d06fa9be66e
getadd321 <ghiwotadane@gmail.com>
2023-04-19 04:01:29 -0700

    function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <std.io>
+#include "function_pointers.h"
+/**
+ * print_name - prints name
+ * @name: string input
+ * @f: pointer to function
+ * Return: nothing
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
