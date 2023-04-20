ed7cce5acd0d3200eff68186ae97d31f22f4aa9c
getadd321 <ghiwotadane@gmail.com>
2023-04-20 14:49:31 -0700

    header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef VARIADIC_FUNCTUNS_H
+#define VARIADIC_FUNCTUNS_H
+
+int _putchar(char c);
+
+int sum_them_all(const unsigned int n, ...);
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
+
+
+#endif /* VARIADIC_FUNCTUNS_H */
