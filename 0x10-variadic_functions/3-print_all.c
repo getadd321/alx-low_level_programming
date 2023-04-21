b9fef4b64909878c64ff9b721ed1d1cffed56279
getadd321 <ghiwotadane@gmail.com>
2023-04-21 15:18:34 -0700

    prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,37 +9,39 @@
 void print_all(const char * const format, ...)
 {
 	int j = 0;
-	char *str;
+	char *str, *spr = "";
 	va_list arg_list;
 
 	va_start(arg_list, format);
-	while (format[j] != '\0')
+	if (format)
 	{
-		switch (format[j])
+		while (format[j] != '\0')
 		{
-			case 'c':
-				printf("%c", va_arg(arg_list, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(arg_list, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(arg_list, double));
-				break;
-			case 's':
-				str = va_arg(arg_list, char*);
-				if (str != NULL)
-					printf("%s", str);
-				else
-					printf("(nil)");
-				break;
-			default:
-				j++;
-				continue;
+			switch (format[j])
+			{
+				case 'c':
+					printf("%s%c", spr, va_arg(arg_list, int));
+					break;
+				case 'i':
+					printf("%s%d", spr, va_arg(arg_list, int));
+					break;
+				case 'f':
+					printf("%s%f", spr, va_arg(arg_list, double));
+					break;
+				case 's':
+					str = va_arg(arg_list, char*);
+					if (str != NULL)
+						printf("%s%s", spr, str);
+					else
+						printf("(nil)");
+					break;
+				default:
+					j++;
+					continue;
+			}
+			spr = ", ";
+			j++;
 		}
-		j++;
-		if (format[j] != '\0')
-			printf(", ");
 	}
 	printf("\n");
 	va_end(arg_list);
